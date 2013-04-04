a3671781fed22fa2f85b7993759b678e7c8cc33b
Francis Chen <kfrancischen@gmail.com>
2013-04-04 13:53:49 +0200

    modified output

--- output.cpp
+++ output.cpp
@@ -4,14 +4,14 @@ This is the CPP for output data
 
 #include <output.h>
 
-Output::Output(int numOfXGrid, int numOfYGrid, double **tempVelocity, int time){
+Output::Output(int numOfXGrid, int numOfYGrid, double **xVelocity, double **yVelocity, int time){
 	ofstream outfile;
 	stringstream outfileName;
 	outfileName << OUTPUT_PATH << "Nx=" << numOfXGrid << "_Ny=" << numOfYGrid << "_t=" << time << ".txt";
 	outfile.open(outfileName.str().c_str());
 	for(int i = 0; i < numOfXGrid; i++){
 		for(int j = 0; j < numOfYGrid; j++){
-			outfile << i << "\t" << j << "\t" << tempVelocity[i][j] << endl;
+			outfile << i << "\t" << j << "\t" << xVelocity[i][j] << "\t" << yVelocity[i][j] << endl;
 		}
 	}
 	outfile.close();
